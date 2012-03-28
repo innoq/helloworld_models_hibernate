1e0c32f8d4aba1da2b452eac837d11f5e1897b36
Stefan Tilkov <stefan.tilkov@innoq.com>
2012-03-28 10:41:39 +0200

    check in of h2 modifications

--- sqlite2h2db.sh
+++ sqlite2h2db.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 rm db/test.*
-echo '.dump' | sqlite3 dbtest.sqlite3 > dbtest-export.sql
-cat dbtest-export.sql \
+echo '.dump' | sqlite3 db/test.sqlite3 > db/test-export.sql
+cat db/test-export.sql \
     | sed -e 's/^PRAGMA.*;//g' \
     | sed -e 's/"\([a-zA-Z_]*\)"/\1/g' \
     | sed -e 's/AUTOINCR/AUTO_INCR/g' | grep -v sqlite | grep -iv unique \
-    > import.sql && java -cp $HOME/.m2/repository/com/h2database/h2/1.3.165/h2-1.3.165.jar \
-                          org.h2.tools.RunScript -url "jdbc:h2:./db/test" -user "sa" -script "./import.sql"
+    > db/import.sql && java -cp $HOME/.m2/repository/com/h2database/h2/1.3.165/h2-1.3.165.jar \
+                          org.h2.tools.RunScript -url "jdbc:h2:./db/test" -user "sa" -script "./db/import.sql"
